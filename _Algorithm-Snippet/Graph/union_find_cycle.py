3b0d516211928d8f765a23aaf88142445a079148
seungriyou <seungri0826@gmail.com>
2023-03-14 15:01:17 +0900

    [SNIPPET] Graph

---
+++ _Algorithm-Snippet/Graph/union_find_cycle.py
@@ -0,0 +1,37 @@
+# 특정 원소가 속한 집합 찾기
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선(union 연산)의 개수 입력 받기
+v, e = map(int, input().split())
+
+# 부모 테이블 상에서 부모를 자기 자신으로 초기화
+parent = [i for i in range(v + 1)]
+
+cycle = False # 사이클 발생 여부
+
+for i in range(e):
+    a, b = map(int, input().split())
+    # 사이클이 발생한 경우(= 루트 노드가 서로 같은 경우) 종료
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    # 사이클이 발생하지 않은 경우, 합집합(union) 수행
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
