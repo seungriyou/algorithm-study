507ddf7a5a03d4639a1ed21f7ed26eb273eb8a17
seungriyou <seungri0826@gmail.com>
2023-03-19 22:57:21 +0900

    [SNIPPET] List

---
+++ _Algorithm-Snippet/List/2_dim_list.py
@@ -0,0 +1,6 @@
+n = 3
+m = 4
+array = [[0] * m for _ in range(n)]
+print(array)
+
+# [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
---
+++ _Algorithm-Snippet/List/rotate_matrix_by_90.py
@@ -0,0 +1,20 @@
+"""2차원 리스트(행렬)를 90도 회전하는 메서드"""
+def rotate_matrix_by_90(a):
+    row_length = len(a)
+    column_length = len(a[0])
+
+    res = [[0] * row_length for _ in range(column_length)]
+    for r in range(row_length):
+        for c in range(column_length):
+            res[c][row_length - 1 - r] = a[r][c]
+
+    return res
+
+# 사용 예시
+a = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+
+print(rotate_matrix_by_90(a))
