370b895a03638e215b12b0ddddd1cd61880bb992
seungriyou <seungri0826@gmail.com>
2023-04-14 11:24:36 +0900

    [이코테] 기출문제 - Greedy

--- 이코테/기출문제/q02_곱하기_혹은_더하기.py
+++ 이코테/기출문제/q02_곱하기_혹은_더하기.py
@@ -1,12 +1,11 @@
 nums = list(map(int, list(input())))
 
 result = nums[0]
-d = nums[0]
 
 # 주의: result 조건도 잊지 않기 (왼, 오 둘다 확인)
 for n in nums[1:]:
     d = n
-    if d in {0, 1} or result <= 1:
+    if d <= 1 or result <= 1:
         result += d
     else:
         result *= d
