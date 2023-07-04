4719ca5f562af59f935e71b3621d2b17b165ec9d
seungriyou <seungri0826@gmail.com>
2023-07-04 23:32:52 +0900

    [LTC] 17 - Letter Combinations of a Phone Number

--- Backtracking/LTC-17.py
+++ Backtracking/LTC-17.py
@@ -1,29 +1,44 @@
-# [LTC] 17 - Letter Combination of a Phone Number
+# [LTC] 17 - Letter Combinations of a Phone Number
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
 from typing import List
 
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # 백트래킹 w/ dfs
+        def dfs(index, path):
+            # print(f"dfs: index={index}, path={path}")
 
-def letterCombinations(digits: str) -> List[str]:
-    def dfs(index: int, path: str) -> None:
-        # 끝까지 탐색하면 백트래킹
-        if len(path) == len(digits):
-            result.append(path)
-            return
+            # 끝까지 탐색하면 result에 추가 및 백트래킹
+            if len(digits) == len(path):
+                result.append(path)
+                return
 
-        # 입력값 자릿수 단위 반복
-        for i in range(index, len(digits)):
-            for j in dic[digits[i]]:
-                dfs(i + 1, path + j)
+            # digits의 자릿수만큼 반복
+            for i in range(index, len(digits)):
+                # 해당 숫자에 해당하는 문자 반복
+                for j in num_mapping[digits[i]]:
+                    dfs(i + 1, path + j)
 
-    # 예외 처리
-    if not digits:
-        return []
+        result = []
 
-    dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-    result = []
-    dfs(0, "")
-    return result
+        if not digits:
+            return result
 
+        num_mapping = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        dfs(0, "")
 
+        return result
+
+sol = Solution()
 digits = "23"
-print(letterCombinations(digits))
+print(sol.letterCombinations(digits))
