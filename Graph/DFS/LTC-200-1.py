6ba2c779a1203bad367a3f6f2bae3014d7cd070a
seungriyou <seungri0826@gmail.com>
2023-07-05 22:38:04 +0900

    [LTC] 200 - Number of Islands (modified)

--- Graph/DFS/LTC-200-1.py
+++ Graph/DFS/LTC-200-1.py
@@ -5,32 +5,27 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+
         def dfs(x, y):
-            row = len(grid)
-            col = len(grid[0])
+            # 범위를 벗어났거나 땅이 아닌 경우 종료
+            if (x < 0 or x >= row or y < 0 or y >= col
+                    or grid[x][y] != "1"):
+                return
 
-            # 범위 확인
-            if x < 0 or x >= row or y < 0 or y >= col:
-                return False
-
-            # 방문하지 않은 경우(= 육지, 1),
-            if grid[x][y] == "1":
-                # 방문 처리
-                grid[x][y] = "0"
-                # 인접 노드 확인
-                dfs(x - 1, y)
-                dfs(x + 1, y)
-                dfs(x, y - 1)
-                dfs(x, y + 1)
-                return True
-            else:
-                return False
+            # 방문하지 않은 경우(if grid[x][y] == "1")
+            # -- 방문 처리
+            grid[x][y] = "0"
+            # -- 인접 노드 확인
+            dfs(x - 1, y)
+            dfs(x + 1, y)
+            dfs(x, y - 1)
+            dfs(x, y + 1)
 
         cnt = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                # if dfs(grid, i, j):
-                #     cnt += 1
+        for i in range(row):
+            for j in range(col):
                 if grid[i][j] == "1":
                     dfs(i, j)
                     cnt += 1
