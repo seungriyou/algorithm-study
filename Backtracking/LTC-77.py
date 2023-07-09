cc5c0bcf862365c1637b1afe89e2b9b1f01c0381
seungriyou <seungri0826@gmail.com>
2023-07-09 22:24:35 +0900

    [LTC] 77 - COmbinations

---
+++ Backtracking/LTC-77.py
@@ -0,0 +1,27 @@
+# [LTC] 77 - COmbinations
+# https://leetcode.com/problems/combinations/
+
+from typing import List
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        results = []
+
+        def dfs(elements, start: int, k: int):
+            # -- 종료 조건
+            if k == 0:
+                results.append(elements[:]) # -- 복사하기
+
+            # -- 반복하기
+            for i in range(start, n + 1):   # -- 전체 숫자 [1, n] 순회
+                elements.append(i)
+                dfs(elements, i + 1, k - 1) # -- i + 1: 이전 선택한 숫자와 겹치지 X (k회 deeper)
+                elements.pop()
+
+        dfs([], 1, k)
+        return results
+
+sol = Solution()
+n = 4
+k = 2
+print(sol.combine(n=4, k=2))
--- Backtracking/README.md
+++ Backtracking/README.md
@@ -3,3 +3,4 @@
 |------|-----|----------|-------|---------|
 | LTC | [17 - Letter Combinations of a Phone Number](https://leetcode.com/problems/letter-combinations-of-a-phone-number/) | 23.07.04 | ✅ | DFS, Backtracking |
 | LTC | [46 - Permutations](https://leetcode.com/problems/permutations/) | 23.07.04 | ✅ | DFS, Backtracking |
+| LTC | [77 - COmbinations](https://leetcode.com/problems/combinations/) | 23.07.09 | ✅ | DFS, Backtracking |
