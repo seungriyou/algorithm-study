44afcf65301b6bc79766dea4f2dd6bf46165a153
seungriyou <seungri0826@gmail.com>
2023-09-02 19:29:34 +0900

    [LTC] 365 - Water and Jug Problem (modified)

--- Graph/BFS/LTC-365.py
+++ Graph/BFS/LTC-365.py
@@ -3,8 +3,41 @@
 
 from collections import deque
 
+
 class Solution:
     def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
+        if targetCapacity > jug1Capacity + jug2Capacity:
+            return False
+
+        q = deque([(0, 0)])  # -- state of two jugs (now, empty jugs)
+        visited = set((0, 0))
+
+        while q:
+            jug1, jug2 = q.popleft()
+            if jug1 + jug2 == targetCapacity:
+                return True
+
+            states = set()
+            states.add((jug1Capacity, jug2))  # -- (1) jug1 채우기
+            states.add((jug1, jug2Capacity))  # -- (2) jug2 채우기
+            states.add((0, jug2))  # -- (3) jug1 버리기
+            states.add((jug1, 0))  # -- (4) jug2 버리기
+            states.add((  # -- (5) jug2를 jug1에 붓기
+                min(jug1 + jug2, jug1Capacity),
+                0 if jug2 < jug1Capacity - jug1 else jug2 - (jug1Capacity - jug1)))
+            states.add((  # -- (6) jug1을 jug2에 붓기
+                0 if jug1 < jug2Capacity - jug2 else jug1 - (jug2Capacity - jug2),
+                min(jug1 + jug2, jug2Capacity)))
+
+            for state in states:
+                if state in visited:
+                    continue
+                q.append(state)
+                visited.add(state)
+
+        return False
+
+    def canMeasureWater2(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
         """
         두 jar 사이에 water를 주고 받는 것은 전혀 상관 X
         물을 각 jar만큼 채우느냐/버리느냐만 중요함
