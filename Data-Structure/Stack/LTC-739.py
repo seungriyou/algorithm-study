f875c42f24b1b47bbc91a1484c9ed983bbb9910d
seungriyou <seungri0826@gmail.com>
2023-09-08 12:10:23 +0900

    [LTC] 739 - Daily Temperatures

--- Data-Structure/Stack/LTC-739.py
+++ Data-Structure/Stack/LTC-739.py
@@ -1,19 +1,22 @@
 # [LTC] 739 - Daily Temperatures
+# https://leetcode.com/problems/daily-temperatures/
 
 from typing import List
 
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        n = len(temperatures)
+        answer = [0] * n
 
-def dailyTemperatures(temperatures: List[int]) -> List[int]:
-    answer = [0] * len(temperatures)
-    stack = []  # index
-    for i, cur in enumerate(temperatures):
-        # 현재 온도가 스택 값보다 높다면 정답 처리
-        while stack and cur > temperatures[stack[-1]]:
-            last = stack.pop()
-            answer[last] = i - last
-        stack.append(i)
+        for i in range(n):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                last = stack.pop()
+                answer[last] = i - last
+            stack.append(i)
 
-    return answer
+        return answer
 
 temperatures = [73,74,75,71,69,72,76,73]
-print(dailyTemperatures((temperatures)))
+sol = Solution()
+print(sol.dailyTemperatures((temperatures)))
--- Data-Structure/Stack/README.md
+++ Data-Structure/Stack/README.md
@@ -1,6 +1,7 @@
 # stack
 | 플랫폼 | 문제 정보 | 날짜       | 리뷰 필요 | 키워드   |
 |-----|-----|----------|-------|-------|
+| LTC | [739 - Daily Temperatures](https://leetcode.com/problems/daily-temperatures/) | | | |
 | LTC | [150 - Evaluate Reverse Polish Notation](https://leetcode.com/problems/evaluate-reverse-polish-notation/) | 23.08.19 | | |
 | LTC | [735 - Asteroid Collision](https://leetcode.com/problems/asteroid-collision/) | 23.07.25 | ✅ | Stack |
 | LTC | [20 - Valid Parentheses](https://leetcode.com/problems/valid-parentheses/) | 23.06.30 |       | Stack |
