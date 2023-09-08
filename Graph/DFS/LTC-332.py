b92ed5e665d503563ae489ebc4a7cf6d4ee79f84
seungriyou <seungri0826@gmail.com>
2023-09-08 15:23:39 +0900

    [LTC] 332 - Reconstruct Itinerary

--- Graph/DFS/LTC-332.py
+++ Graph/DFS/LTC-332.py
@@ -1,25 +1,27 @@
 # [LTC] 332 - Reconstruct Itinerary
+# https://leetcode.com/problems/reconstruct-itinerary/
 
 from typing import List
 from collections import defaultdict
 
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # === DFS ===
+        graph = defaultdict(list)
+        for s, e in sorted(tickets, reverse=True):  # -- lexical order의 반대로 정렬
+            graph[s].append(e)
 
-def findItinerary(tickets: List[List[str]]) -> List[str]:
-    graph = defaultdict(list)
-    for a, b in sorted(tickets, reverse=True):  # 사전식 순서의 반대로 sort 하면 pop(0) 대신 pop() 사용 가능
-        graph[a].append(b)
+        route = []
 
-    route = []
-    def dfs(a):
-        while graph[a]:
-            # dfs(graph[a].pop(0))    # 어휘순 방문해야 하므로 pop(0)으로 첫 번째 원소 pop
-            dfs(graph[a].pop())
-        route.append(a) # 역순으로 생성될 것
-        # print(route)
+        def dfs(start):
+            while graph[start]:
+                dfs(graph[start].pop())  # -- 반대로 정렬했으므로 뒤쪽부터 pop 가능
+            route.append(start)
 
-    dfs('JFK')
-    return route[::-1]
+        dfs('JFK')
+        return route[::-1]
 
 
 tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
-print(findItinerary(tickets))
+sol = Solution()
+print(sol.findItinerary(tickets))
