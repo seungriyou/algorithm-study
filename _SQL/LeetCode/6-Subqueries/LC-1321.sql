cad83f8a9dd235679d34db0c107dc0d453e18de8
seungriyou <seungri0826@gmail.com>
2023-12-09 10:17:55 +0900

    [SQL] Cracking LeetCode (REVIEW) (23.12.09)

--- _SQL/LeetCode/6-Subqueries/LC-1321.sql
+++ _SQL/LeetCode/6-Subqueries/LC-1321.sql
@@ -13,3 +13,37 @@ WHERE
     AND DATEDIFF(C1.visited_on, C2.visited_on) BETWEEN 0 AND 6
 GROUP BY C1.visited_on
 ORDER BY C1.visited_on;
+
+
+# ===== (23.12.09) reviewed =====
+# ===== w/o window function, w/ join =====
+WITH DAILY_AMOUNT AS (
+    SELECT visited_on, SUM(amount) AS daily_amount
+    FROM Customer
+    GROUP BY 1
+)
+
+SELECT
+    D1.visited_on,
+    SUM(D2.daily_amount) AS amount,
+    ROUND(SUM(D2.daily_amount) / 7, 2) AS average_amount
+FROM DAILY_AMOUNT D1, DAILY_AMOUNT D2
+WHERE
+    D1.visited_on >= DATE_ADD((SELECT MIN(visited_on) FROM DAILY_AMOUNT), INTERVAL 6 DAY) AND   # -- 최소 visited_on으로부터 6일 후 날짜부터만
+    DATEDIFF(D1.visited_on, D2.visited_on) BETWEEN 0 AND 6  # -- D1.visited_on에 대해 0~6일전 D2.visited_on만 남기기
+GROUP BY 1
+ORDER BY 1;
+
+# ===== w/ window function =====
+SELECT
+    visited_on,
+    amount,
+    ROUND(amount / 7, 2) AS average_amount
+FROM (
+    SELECT
+        DISTINCT visited_on,
+        SUM(amount) OVER (ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) AS amount
+    FROM Customer
+) C
+WHERE visited_on >= DATE_ADD((SELECT MIN(visited_on) FROM Customer), INTERVAL 6 DAY)
+ORDER BY 1;
--- _SQL/LeetCode/7-Advanced_String_Regex_Clause/LC-1327.sql
+++ _SQL/LeetCode/7-Advanced_String_Regex_Clause/LC-1327.sql
@@ -16,3 +16,11 @@ INNER JOIN Products P ON O.product_id = P.product_id;
 # WHERE YEAR(O.order_date) = '2020' AND MONTH(O.order_date) = '02'
 # GROUP BY P.product_id
 # HAVING unit >= 100;
+
+# ===== (23.12.09) reviewed =====
+SELECT product_name, SUM(unit) AS unit
+FROM Orders
+INNER JOIN Products USING(product_id)
+WHERE DATE_FORMAT(order_date, '%Y-%m') = '2020-02'
+GROUP BY product_id
+HAVING unit >= 100; # MySQL에서 HAVING 절은 SELECT 절이나 outer subqueries에 있는 columns도 참조 가능
